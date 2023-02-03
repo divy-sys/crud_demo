c159beafbe4798221b67bd6ae24b0b2c07ad5222
divy-sys <divy.varia.1020@gmail.com>
2023-02-03 17:46:57 +0530

    Controller code with DRY

--- app/controllers/persons_controller.rb
+++ app/controllers/persons_controller.rb
@@ -1,17 +1,20 @@
 class PersonsController < ApplicationController 
+  before_action :find_person, only: [:show, :edit, :update, :destroy]
   def index
+    @persons = Person.all
   end
 
   def show
-
   end
 
   def new
-
+    @person = Person.new 
   end
 
   def create
-  
+    @person = Person.new(person_params)
+    @person.save 
+    redirect_to persons_path
   end
 
   def edit 
@@ -19,9 +22,22 @@ class PersonsController < ApplicationController
   end
 
   def update
-
+    @person.update(person_params) 
+    redirect_to persons_path
   end
   
   def destroy
+    @person.destroy
+    redirect_to persons_path
+  end
+
+  private
+
+  def find_person
+    @person = Person.find_by_id(params[:id])
+  end
+
+  def person_params
+    params.require(:person).permit(:name, :age, :address)
   end
 end
